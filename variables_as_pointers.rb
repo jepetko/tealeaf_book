cccfb886090e5cc84f5176dd7d09c68e6de885a4
katarina <golbang.k@gmail.com>
2014-10-10 11:55:10 +0200

    more stuff, operations mutating the caller

---
+++ variables_as_pointers.rb
@@ -0,0 +1,16 @@
+a = 'hello'
+b = 'there'
+b = a
+a = 'hello the next time'
+p "a: #{a}"
+p "b: #{b}"
+p '----------------------'
+c = 'I will'
+d = 'hit you'
+d = c
+c << ' hit you too'
+p "c: #{c}"
+p "d: #{d}"
+
+
+
