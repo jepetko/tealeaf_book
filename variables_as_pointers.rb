b9f74369f1059ef5c60f3a92a6d39f349205049c
katarina <golbang.k@gmail.com>
2014-10-10 14:54:11 +0200

    destructive changes of objects

--- variables_as_pointers.rb
+++ variables_as_pointers.rb
@@ -13,4 +13,21 @@ p "c: #{c}"
 p "d: #{d}"
 
 
+# uniq vs. uniq!
 
+arr = [1,2,3,3,3,4,5]
+arr_clone = arr.uniq
+p "orig: #{arr}"
+p "clone: #{arr_clone}"
+arr_clone = arr.uniq!
+p "orig: #{arr}"
+p "clone: #{arr_clone}"
+
+# map vs. map!
+arr2 = ['A', 'B', 'B', 'C']
+arr2_clone = arr2.map {|el| el + '*'}
+p arr2
+p arr2_clone
+arr2_clone = arr2.map! {|el| el + '*'}
+p arr2
+p arr2_clone
